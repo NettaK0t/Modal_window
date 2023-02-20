b7ba884faeccee8cb96633d6f11732f129a9635e
NettaK0t <110839339+NettaK0t@users.noreply.github.com>
2023-02-20 10:44:10 +0300

    add script.js

---
+++ script.js
@@ -0,0 +1,30 @@
+'use strict';
+const modalWindow = document.querySelector(`.modal-window`);
+const overlay = document.querySelector(`.overlay`);
+const btnCloseModalWindow = document.querySelector(`.close-modal-window`);
+const btnsShowModalWindow = document.querySelectorAll(`.show-modal-window`);
+
+const showModalWindow = function () {
+  modalWindow.classList.remove(`hidden`);
+  overlay.classList.remove(`hidden`);
+};
+
+const closeModalWindow = function () {
+  modalWindow.classList.add(`hidden`);
+  overlay.classList.add(`hidden`);
+};
+
+for (let i = 0; i < btnsShowModalWindow.length; i++) {
+  btnsShowModalWindow[i].addEventListener(`click`, showModalWindow);
+  //modalWindow.style.display = `block`;
+}
+
+btnCloseModalWindow.addEventListener(`click`, closeModalWindow);
+overlay.addEventListener(`click`, closeModalWindow);
+
+document.addEventListener(`keydown`, function (e) {
+  if (e.key === `Escape` && !modalWindow.classList.contains(`hidden`)) {
+    //if (!modalWindow.classList.contains(`hidden`))
+    closeModalWindow();
+  }
+});
